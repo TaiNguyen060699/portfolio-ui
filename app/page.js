4ba6b9d695cda03208af867174b07384401438c3
TaiNguyen <ntai8463@gmail.com>
2023-11-14 17:08:21 +0700

    Fix format code

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Header from '@/components/TheHeader';
 import Experience from '@/components/Experience';
- import Project from '@/components/Project';
+import Project from '@/components/Project';
 import Footer from '@/components/TheFooter';
 
 export default function Home() {
