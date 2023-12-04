5ece5738e25e0748395280120c0fa775deec4594
TaiNguyen <ntai8463@gmail.com>
2023-12-04 08:48:29 +0700

    [Fix]

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Header from '@/components/TheHeader';
-import Experience from '@/components/Experience';
-import Project from '@/components/Project';
 import Footer from '@/components/TheFooter';
+import Experience from './../components/Experience';
+import Project from './../components/Project';
 
 export default function Home() {
   return (
